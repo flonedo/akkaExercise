2edd9b704732299bfdd5bc978d17fc2acf752c05
Francesca Lonedo <lonedofrancesca@gmail.com>
2019-08-27 16:28:26 +0200

    added implementation of a person entity

---
+++ bank-accounts/src/main/scala/bank/domain/Person.scala
@@ -0,0 +1,49 @@
+package bank.domain
+
+import bank.domain.Domain.{DomainCommand, DomainEntity, DomainEvent}
+
+case class Person(fullName: String, bankAccounts: Vector[BankAccount]) extends DomainEntity
+
+object Person {
+
+  sealed trait PersonEvent extends DomainEvent[Person] {
+    val fullName: String
+  }
+
+  sealed trait PersonCommand extends DomainCommand[Person, PersonEvent]{
+    val fullName: String
+  }
+
+  case class CloseBankAccount(fullName: String, bankAccount: BankAccount) extends PersonCommand {
+    override def applyTo(domainEntity: Person): Either[String, Option[PersonEvent]] = {
+      if (fullName == domainEntity.fullName) {
+        if (domainEntity.bankAccounts.contains(bankAccount)) {
+          Right(Some(ClosedBankAccount(fullName, bankAccount)))
+        }
+        else Right(None)
+      }
+      else Left("Wrong person")
+    }
+  }
+
+  case class ClosedBankAccount(fullName: String, bankAccount: BankAccount) extends PersonEvent {
+    override def applyTo(domainEntity: Person): Person =
+      domainEntity.copy(bankAccounts = domainEntity.bankAccounts.filter(_ != bankAccount))
+  }
+
+  case class OpenBankAccount(fullName: String) extends PersonCommand {
+    override def applyTo(domainEntity: Person): Either[String, Option[PersonEvent]] = {
+      if (fullName == domainEntity.fullName) {
+        Right(Some(OpenedBankAccount(fullName)))
+      }
+      else Left("Wrong person")
+    }
+  }
+
+  case class OpenedBankAccount(fullName: String) extends PersonEvent {
+    override def applyTo(domainEntity: Person): Person =
+      domainEntity.copy(bankAccounts = domainEntity.bankAccounts :+ BankAccount(iban = scala.util.Random.nextInt(22).toString, balance = 0))
+  }
+  
+
+}
