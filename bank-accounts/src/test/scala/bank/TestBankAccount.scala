705d02aab9a5fe8b236e60dfe6dd9d375085f327
Marco <marco.paggioro@gmail.com>
2019-08-28 10:46:27 +0200

    Added test for BankAccount

--- bank-accounts/src/test/scala/bank/TestBankAccount.scala
+++ bank-accounts/src/test/scala/bank/TestBankAccount.scala
@@ -1,5 +1,48 @@
 package bank
+import bank.domain.BankAccount
+import org.scalatest.{EitherValues, FunSuite}
 
-class TestBankAccount {
+class TestBankAccount extends FunSuite with EitherValues {
 
+	def accountWithMoney(iban: String) = BankAccount(iban, 10000.0)
+	def accountWithoutMoney(iban: String) = BankAccount(iban, 0.0)
+
+	test("Deposit money into an empty bank account") {
+		val account = accountWithoutMoney("test")
+		for(x <- 1 to 100) {
+			val x = scala.util.Random.nextInt(1000)
+			val action = BankAccount.Deposit(account.iban, x).applyTo(account)
+			assert(action.isRight, "Error while depositing money into empty account")
+		}
+	}
+
+	test("Deposit money into a non-empty bank account") {
+		val account = accountWithMoney("test2")
+		for(x <- 1 to 100) {
+			val x = scala.util.Random.nextInt(1000)
+			val action = BankAccount.Deposit(account.iban, x).applyTo(account)
+			assert(action.isRight, "Error while depositing money into non-empty account")
+		}
+	}
+
+	test("Withdraw money from an empty account") {
+		val account = accountWithoutMoney("test3")
+		for(x <- 1 to 100) {
+			val x = scala.util.Random.nextInt(1000)
+			val action = BankAccount.Withdraw(account.iban, x).applyTo(account)
+			assert(action.isLeft, "Error while withdrawing money from an empty account")
+		}
+	}
+
+	test("Withdraw money from a non-empty account") {
+		val account = accountWithoutMoney("test4")
+		for(x <- 1 to 100) {
+			val x = scala.util.Random.nextInt(5000)
+			val action = BankAccount.Withdraw(account.iban, x).applyTo(account)
+			if(x > account.balance)
+				assert(action.isLeft, "Error while withdrawing money from a non-empty account")
+			else
+				assert(action.isRight, "Error while withdrawing money from a non-empty account")
+		}
+	}
 }
