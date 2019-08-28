841a6b95dfed9da8dfd2ee42506855ba91f96dec
Marco <marco.paggioro@gmail.com>
2019-08-28 14:14:53 +0200

    Test of BankAccount more accurate

--- bank-accounts/src/test/scala/bank/TestBankAccount.scala
+++ bank-accounts/src/test/scala/bank/TestBankAccount.scala
@@ -4,57 +4,64 @@ import org.scalatest.FunSuite
 
 class TestBankAccount extends FunSuite {
 
-	def accountWithMoney(iban: String) = BankAccount(iban, 10000.0)
-	def accountWithoutMoney(iban: String) = BankAccount(iban, 0.0)
+	def accountWithMoney(iban: String) = BankAccount(iban, 100000.0)
 
-	test("Deposit money into an empty bank account") {
-		val account = accountWithoutMoney("test")
-		for(x <- 1 to 100) {
-			val x = scala.util.Random.nextInt(1000)
-			val action = BankAccount.Deposit(account.iban, x).applyTo(account)
-			assert(action.isRight, "Error while depositing money into empty account")
-		}
-	}
 
-	test("Deposit money into a non-empty bank account") {
-		val account = accountWithMoney("test2")
-		for(x <- 1 to 100) {
-			val x = scala.util.Random.nextInt(1000)
-			val action = BankAccount.Deposit(account.iban, x).applyTo(account)
-			assert(action.isRight, "Error while depositing money into non-empty account")
-		}
-	}
-
-	test("Withdraw money from an empty account") {
-		val account = accountWithoutMoney("test3")
-		for(x <- 1 to 100) {
-			val x = scala.util.Random.nextInt(1000)
-			val action = BankAccount.Withdraw(account.iban, x).applyTo(account)
-			assert(action.isLeft, "Error while withdrawing money from an empty account")
-		}
-	}
-
-	test("Withdraw money from a non-empty account") {
-		val account = accountWithoutMoney("test4")
-		for(x <- 1 to 100) {
-			val x = scala.util.Random.nextInt(5000)
-			val action = BankAccount.Withdraw(account.iban, x).applyTo(account)
-			if(x > account.balance)
-				assert(action.isLeft, "Error while withdrawing money from a non-empty account")
-			else
-				assert(action.isRight, "Error while withdrawing money from a non-empty account")
-		}
-	}
-
-	test("Deposit money into an empty bank account with wrong IBAN") {
-		val account = accountWithoutMoney("test4")
+	test("Deposit money with wrong IBAN") {
+		val account = accountWithMoney("test")
 		val action = BankAccount.Deposit("wrong", 10).applyTo(account)
-		assert(action.isLeft, "Error while depositing money into an empty bank account with wrong IBAN")
+		assert(action.left.get equals "Wrong IBAN")
 	}
 
-	test("Withdraw money into an empty bank account with wrong IBAN") {
-		val account = accountWithoutMoney("test5")
+	test("Withdraw money with wrong IBAN") {
+		val account = accountWithMoney("test")
 		val action = BankAccount.Withdraw("wrong", 10).applyTo(account)
-		assert(action.isLeft, "Error while withdrawing money into an empty bank account with wrong IBAN")
+		assert(action.left.get equals "Wrong IBAN")
+	}
+
+	test("Withdraw money") {
+		def testing(n: Int, account: BankAccount): Unit = {
+			if(n == 0) return
+			//Do action
+			val x = scala.util.Random.nextInt(1000) - scala.util.Random.nextInt(500)
+			val action = BankAccount.Withdraw(account.iban, x).applyTo(account)
+
+			//Testing
+			if (x > 0) {
+				if (account.balance >= x) {
+					def evento = action.right.get.get
+					assert(account.balance - x == evento.applyTo(account).balance, "Error balance")
+					testing(n - 1, evento.applyTo(account))
+				}
+				else assert(action.left.get equals "Not enought money")
+			} else {
+				if (x == 0) assert(action.right.get equals None)
+				else assert(action.left.get equals "Negative amount")
+				testing(n - 1, account)
+			}
+
+		}
+		testing(10000, accountWithMoney("test"))
+	}
+
+	test("Deposit money") {
+		def testing(n: Int, account: BankAccount): Unit = {
+			if (n == 0) return
+			//Do action
+			val x = scala.util.Random.nextInt(1000) - scala.util.Random.nextInt(500)
+			val action = BankAccount.Deposit(account.iban, x).applyTo(account)
+
+			//Testing
+			if (x > 0) {
+				def evento = action.right.get.get
+				assert(account.balance + x == evento.applyTo(account).balance, "Error balance")
+				testing(n - 1, evento.applyTo(account))
+			} else {
+				if (x == 0) assert(action.right.get equals None)
+				else assert(action.left.get equals "Negative amount")
+				testing(n - 1, account)
+			}
+		}
+		testing(10000, accountWithMoney("test"))
 	}
 }
