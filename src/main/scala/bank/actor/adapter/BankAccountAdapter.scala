571d8ceb2ad463efcd8edcbeea7cec9d38d1e7c4
Francesca Lonedo <lonedofrancesca@gmail.com>
2019-10-02 17:58:50 +0200

    added event adapter for cassandra journal

--- src/main/scala/bank/actor/adapter/BankAccountAdapter.scala
+++ src/main/scala/bank/actor/adapter/BankAccountAdapter.scala
@@ -1,3 +1,24 @@
-package bank.actor.adapter class BankAccountAdapter {
+package bank.actor.adapter
+
+import akka.persistence.journal.{EventAdapter, EventSeq, Tagged}
+import bank.actor.write.BankAccountWriterActor
+import bank.domain.BankAccount
+
+class BankAccountAdapter extends EventAdapter {
+  override def manifest(event: Any): String = event.getClass.getSimpleName
+
+  val tag = Set(BankAccountWriterActor.bankAccountDetailsTag)
+  override def fromJournal(event: Any, manifest: String): EventSeq = event match {
+    case event: BankAccount.Created   => EventSeq.single(event)
+    case event: BankAccount.Deposited => EventSeq.single(event)
+    case event: BankAccount.Withdrawn => EventSeq.single(event)
+  }
+
+  override def toJournal(event: Any): Any = event match {
+    case event: BankAccount.Created   => Tagged(event, tag)
+    case event: BankAccount.Deposited => Tagged(event, tag)
+    case event: BankAccount.Withdrawn => Tagged(event, tag)
+
+  }
 
 }
