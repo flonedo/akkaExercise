44ba5a4b75c35fcad30b6b3831c6942ca3813254
Marco <marco.paggioro@gmail.com>
2019-10-04 12:34:36 +0200

    common exporter

---
+++ src/main/scala/bank/actor/projector/export/CommonExporter.scala
@@ -0,0 +1,40 @@
+package bank.actor.projector.export
+
+import java.io.FileWriter
+import java.util.UUID
+
+import akka.persistence.query.TimeBasedUUID
+import bank.actor.projector.ProjectionIndexer
+
+import scala.io.Source
+import scala.util.{Failure, Try}
+
+trait CommonExporter[T] extends ProjectionIndexer[T] {
+
+  val offsetFilePath: String
+
+  override def readOffset(): Option[TimeBasedUUID] = {
+    val offset = Source.fromFile(offsetFilePath)
+    offset match {
+      case defined if offset.nonEmpty =>
+        val offsetString = defined.getLines().mkString
+        Try(TimeBasedUUID(UUID.fromString(offsetString))) match {
+          case uuid if uuid.isSuccess => Some(uuid.get)
+          case Failure(_) => None
+        }
+      case _ => None
+    }
+  }
+
+  def writeOffset(offset: TimeBasedUUID): Either[Exception, TimeBasedUUID] = {
+    val offsetFw = new FileWriter(offsetFilePath, false)
+    try {
+      offsetFw.write(s"${offset.value}")
+      Right(offset)
+    } catch {
+      case e: Exception => Left(e)
+    } finally {
+      offsetFw.close()
+    }
+  }
+}
