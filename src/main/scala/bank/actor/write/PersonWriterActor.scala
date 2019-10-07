1089d8024dc5ac20091a89ee48d69a1ebf1c0526
Marco <marco.paggioro@gmail.com>
2019-10-07 10:42:56 +0200

    PersonWriter aligned with BankAccountWriter

--- src/main/scala/bank/actor/write/PersonWriterActor.scala
+++ src/main/scala/bank/actor/write/PersonWriterActor.scala
@@ -1,43 +1,47 @@
 package bank.actor.write
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props, ReceiveTimeout}
 import akka.cluster.sharding.ShardRegion
-import akka.persistence.{PersistentActor, SnapshotOffer}
+import akka.event.LoggingReceive
+import akka.persistence.{PersistentActor, RecoveryCompleted, SnapshotOffer}
 import bank.actor.Messages.Done
 import bank.domain.Person
 import bank.domain.Person.PersonCommand
 
 import scala.concurrent.duration._
 
-class PersonWriterActor() extends Actor with ActorSharding with PersistentActor with ActorLogging {
+class PersonWriterActor extends Actor with ActorSharding with PersistentActor with ActorLogging {
 
   override implicit val system: ActorSystem = context.system
 
   override def persistenceId: String = s"${PersonWriterActor.name}-${self.path.name}"
 
-  context.setReceiveTimeout(120.seconds)
-
   var state: Person = Person.empty
+
+  context.setReceiveTimeout(120.seconds)
+  private def receivePassivate: Receive = {
+    case ReceiveTimeout        => context.parent ! ShardRegion.Passivate
+    case ShardRegion.Passivate => context.stop(self)
+  }
 
   override def receiveCommand: Receive = {
     case personOperation: Person.PersonCommand => {
       personOperation.applyTo(state) match {
 
-        case Right(Some(event: Person.PersonEvent)) => {
+        case Right(Some(event)) => {
           persist(event) { _ =>
             state = update(state, event)
             log.info(event.toString)
             if (lastSequenceNr % snapShotInterval == 0 && lastSequenceNr != 0) {
               saveSnapshot(state)
-              sender() ! Done
             }
+            sender() ! Done
           }
         }
 
         case Right(None) => {
           sender() ! Done
         }
-
         case Left(error) => {
           sender() ! error
         }
@@ -49,12 +53,14 @@ class PersonWriterActor() extends Actor with ActorSharding with PersistentActor 
 
   val snapShotInterval = 10
 
-  override val receiveRecover: Receive = {
-
-    case event: Person.PersonEvent => update(state, event)
-
+  override def receiveRecover: Receive = receivePassivate orElse LoggingReceive {
+    case RecoveryCompleted => log.info("Recovery completed!")
+    case event: Person.PersonEvent =>
+      state = update(state, event)
     case SnapshotOffer(_, snapshot: Person) => state = snapshot
+    case unknown                                 => log.error(s"Received unknown message in receiveRecover:$unknown")
   }
+
 }
 
 object PersonWriterActor {
@@ -62,6 +68,8 @@ object PersonWriterActor {
   val numberOfShards = 100
 
   val name = "person-writer-actor"
+
+  val personDetailsTag: String = "person"
 
   def props(): Props = Props(new PersonWriterActor())
 
@@ -74,7 +82,7 @@ object PersonWriterActor {
       (math.abs(entityId.hashCode()) % numberOfShards).toString
 
     {
-      case m: PersonCommand            => computeShardId(m.fullName)
+      case m: PersonCommand       => computeShardId(m.fullName)
       case ShardRegion.StartEntity(id) => computeShardId(id)
     }
   }
